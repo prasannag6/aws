f93a8bf858965612d456dbbf414cb4adb9273071
Prasanna Gunasekaran <prasannag.vmware@gmail.com>
2023-11-06 00:57:44 +0000

    smjs region

--- streamlitapp/smjs.py
+++ streamlitapp/smjs.py
@@ -1,6 +1,6 @@
 import boto3, json
 #import constants as const
-
+region_name = 'us-east-1'
 # endpoint_name = const.smjs_endpoint
 endpoint_name = "<Endpoint-name>"
 
@@ -13,11 +13,11 @@ DO_SAMPLE = True
 CONTENT_TYPE = 'application/json'
 
 def generate_response(prompt, temperature, top_p, max_new_tokens):
-    session = boto3.Session()
+    session = boto3.Session(region_name=region_name)
     smclient = session.client('sagemaker-runtime')
 
 
-    content_type = 'application/json'                                        # The MIME type of the input data in the request body.
+    content_type = 'application/json'
     input_data = {
         "inputs": prompt,
         "parameters": {
